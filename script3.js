334c4d93e2ada0b442a5baeab4b4c7b74d59ff06
Marshall-67 <109167755+Marshall-67@users.noreply.github.com>
2024-12-07 22:14:20 +0000

    Create script3.js

---
+++ script3.js
@@ -0,0 +1,19 @@
+function toggleSection(sectionId) {
+  const section = document.getElementById(sectionId);
+  if (section.style.display === "none" || section.style.display === "") {
+    section.style.display = "block";
+  } else {
+    section.style.display = "none";
+  }
+}
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById('espSection').style.display = 'block';
+  document.getElementById('miscSection').style.display = 'block';
+});
+
+
+// Initialize jscolor with hideValue option
+jscolor.presets.default = {
+  hideValue: true
+};
+jscolor.install();
